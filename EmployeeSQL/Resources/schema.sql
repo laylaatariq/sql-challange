ce153d6750d5da0842a9673fae3e2937d559df4a
Laylaa Tariq <laaylatariq@gmail.com>
2019-12-09 10:01:24 -0600

    completed schema file

--- EmployeeSQL/Resources/schema.sql
+++ EmployeeSQL/Resources/schema.sql
@@ -15,4 +15,41 @@ CREATE TABLE departments(
 --Creating dept_emp table
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no )
+	dept_no VARCHAR NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL
+);
+
+--Creating dept_manager table
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL
+);
+
+--Creating employee table
+CREATE TABLE employee(
+	emp_no INT NOT NULL,
+	birth_date date NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date date NOT NULL
+);
+
+--Creating salaries table
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL
+);
+
+--Creating titles table
+CREATE TABLE title(
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL
+);
