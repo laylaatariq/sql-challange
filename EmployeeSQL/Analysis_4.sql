d3c4d62798034c52dfdfa162b8b387e21fdb8259
Laylaa Tariq <laaylatariq@gmail.com>
2019-12-16 09:13:44 -0600

    completed analysis 4, now going to start analysis 5, after this 3 more to go

---
+++ EmployeeSQL/Analysis_4.sql
@@ -0,0 +1,7 @@
+create VIEW employee_department as
+select de.emp_no,
+	e.first_name,
+	e.last_name,
+	d.dept_name
+from employee as e, departments as d, dept_emp as de
+where de.emp_no = e.emp_no and de.dept_no = d.dept_no;
